68d70772d7c41ea86402ae3f411c595d08cdbb9c
Leandro Wagner <lsw_wagner@yahoo.com.br>
2022-02-04 22:43:03 -0300

    estrutura inicial

---
+++ mariadb/sql/init-db.sql
@@ -0,0 +1,12 @@
+USE testdb;
+
+CREATE TABLE test (
+        name varchar(30),
+        email varchar(50)
+);
+
+INSERT INTO test (name, email)
+VALUES
+        ('Ahmer', 'ahmer@centlinux.com'),
+        ('Mansoor','mansoor@centlinux.com'),
+        ('Salman','salman@centlinux.com');
